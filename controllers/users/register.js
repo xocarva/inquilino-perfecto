3e84a7f299c74130f39585798beba1f6537e10f9
xocarva <correo@xoancarneiro.gal>
2022-01-01 19:26:27 +0100

    added user validator & encrypted pass & random activation code

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,16 +1,49 @@
+const bcrypt = require("bcrypt")
+const crypto = require('crypto')
+
 const { usersRepository } = require('../../repository/index')
+const userValidator = require('../../validators/userSchema')
+const SALT_ROUNDS = Number(process.env.SALT_ROUNDS)
 
 const register = async (req, res) => {
     const user = req.body
 
-    if (!user) {
-      res.status(400)
-      res.end('You should provide a valid user to save')
-    } else {
-      res.status(200)
-      const insertId = await usersRepository.saveUser(user)
-      res.send(`Usuario con id ${insertId} guardado y pendiente de verificar`)
+    try {
+        await userValidator.validateAsync(user)
+    } catch (error) {
+        res.status(400)
+        res.end(error.mesage)
+        return
     }
+
+    // if (usersRepository.userExists(user)) {
+    //   res.status(403)
+    //   res.end('User already exists')
+    //   return
+    // }
+
+    let encryptedPassword
+
+    try {
+        encryptedPassword = await bcrypt.hash(user.password, SALT_ROUNDS)
+
+    } catch (error) {
+        res.status(500)
+        res.end(error.message)
+        return
+    }
+    const activationCode = crypto.randomBytes(16).toString('hex')
+   try {
+        await usersRepository.saveUser({ ...user, password: encryptedPassword, activationCode })
+   } catch (error) {
+        res.status(400)
+        res.end(error.message)
+        return
+   }
+
+    res.status(200)
+    res.send(`Usuario guardado y pendiente de activar`)
+
 }
 
 module.exports = register
