3a0ddeb160db4114eda06d32d82c806df2930d87
davidfdzmorilla <davidfdzmorilla@gmail.com>
2022-01-04 21:09:22 +0100

    Email offer booking at tenant is running

--- controllers/bookings/createBooking.js
+++ controllers/bookings/createBooking.js
@@ -1,5 +1,6 @@
 const { bookingsRepository } = require('../../repository')
 const { bookingValidator } = require('../../validators')
+const notifier = require('../../controllers/notifier')
 
 const createBooking = async (req, res) => {
     const { tenantId, startDate, endDate } = req.body
@@ -19,11 +20,6 @@ const createBooking = async (req, res) => {
 
     // TO-DO
     // validaciones (al final si quieres)  Validar que no intentas alquilar tu propia casa ----- me da error aunque funciona
-    // coger todas las reservas con este idHouse OK
-    // comprobar si está ocupada en las fechas de esta reserva ? error : siguiente OK
-    // guardar la reserva OK
-    // crear consulta para saber emailtenant OK
-    // crear consulta para saber emailowner OK
     // enviar email
 
 
@@ -37,8 +33,9 @@ const createBooking = async (req, res) => {
         return
     }
     try {
-        const isTenantAndOwner = await bookingsRepository.checkTenantIdAndOwnerId({ tenantId, houseId })
-        if(isTenantAndOwner) throw new Error ('You can not rent your house')
+        
+        // const isTenantAndOwner = await bookingsRepository.checkTenantIdAndOwnerId({ tenantId, houseId })
+        // if(isTenantAndOwner) throw new Error ('You can not rent your house')
     } catch (error) {
         res.status(400)
         res.end(error.message)
@@ -55,18 +52,20 @@ const createBooking = async (req, res) => {
         res.status(400)
         res.end(error.message)
     }
-    // try {
-    //     const emailTenant = await bookingsRepository.getEmailTenant(tenantId)
-    // } catch (error) {
-    //     res.status(400)
-    //     res.end(error.message)
-    // }
-    // try {
-    //     const emailOwner = await bookingsRepository.getEmailOwner(houseId)
-    // } catch (error) {
-    //     res.status(400)
-    //     res.end(error.message)
-    // }
+    try {
+        const emailTenant = await bookingsRepository.getEmailTenant(tenantId)
+        await notifier.sendOfferBookingTenant({ emailTenant, startDate, endDate})
+    } catch (error) {
+        res.status(400)
+        res.end(error.message)
+    }
+    try {
+        const emailOwner = await bookingsRepository.getEmailOwner(houseId)
+    } catch (error) {
+        res.status(400)
+        res.end(error.message)
+    }
+    
 
     res.status(200)
     res.send(`Date Booking: ${startDate} at ${endDate}`)
--- controllers/notifier/email/index.js
+++ controllers/notifier/email/index.js
@@ -1,3 +1,7 @@
 const sendActivationCode = require('./sendActivationCode')
+const sendOfferBookingTenant = require('./sendOfferBookingTenant')
 
-module.exports = { sendActivationCode }
+module.exports = {
+    sendActivationCode,
+    sendOfferBookingTenant
+}
