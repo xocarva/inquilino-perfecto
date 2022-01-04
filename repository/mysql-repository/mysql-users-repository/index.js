57c0ca43aec23affbecf0d3432b60be20bba0817
xocarva <correo@xoancarneiro.gal>
2022-01-04 15:02:05 +0100

    added post-users-login

--- repository/mysql-repository/mysql-users-repository/index.js
+++ repository/mysql-repository/mysql-users-repository/index.js
@@ -3,11 +3,14 @@ const userExists = require('./userExists')
 const getUsersByActivationCode = require('./getUsersByActivationCode')
 const activateUser = require('./activateUser')
 const rateBooking = require('./rateBooking')
+const getUserByEmail = require('./getUserByEmail')
+
 
 module.exports = {
     saveUser,
     userExists,
     getUsersByActivationCode,
     activateUser,
-    rateBooking
+    rateBooking,
+    getUserByEmail
 }
