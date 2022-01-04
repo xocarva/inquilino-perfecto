1dcb8d1f4fa34ad0b02a1dfef30b8fcf5d93e638
xocarva <correo@xoancarneiro.gal>
2022-01-04 14:05:09 +0100

    routes setted

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { register, validate, rate } = require('../controllers/users')
+const { register, validate, rate, login } = require('../controllers/users')
 
 router.get('/validate/:activationCode', validate)
 router.post('/register', register)
+router.post('/login', login)
 router.post('/rate/:bookingId', rate)
 
 module.exports = router
