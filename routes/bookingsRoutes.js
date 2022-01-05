be3a88af8f62d829887d627096d97748f419f2f2
davidfdzmorilla <davidfdzmorilla@gmail.com>
2022-01-05 19:45:27 +0100

    confirmBoking finished

--- routes/bookingsRoutes.js
+++ routes/bookingsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const isAuthorized = require('../middlewares/isAuthorized')
 const { createBooking, confirmBooking } = require('../controllers/bookings')
 
 
-router.post('/:houseId', createBooking)
-router.put('/confirm/:bookingId', confirmBooking)
+router.post('/:houseId', isAuthorized, createBooking)
+router.put('/confirm/:bookingId', isAuthorized, confirmBooking)
 
 
 module.exports = router
