242b907d3456d26d1e59d4126611ffd782f49957
davidfdzmorilla <davidfdzmorilla@gmail.com>
2022-01-05 13:03:42 +0100

    confirmBoking finished

--- controllers/notifier/email/index.js
+++ controllers/notifier/email/index.js
@@ -1,9 +1,13 @@
 const sendActivationCode = require('./sendActivationCode')
 const sendBookingOfferPenddingTenant = require('./sendBookingOfferPenddingTenant')
 const sendBookingOfferPenddingOwner = require('./sendBookingOfferPenddingOwner')
+const sendConfirmBookingTenant = require('./sendConfirmBookingTenant')
+const sendConfirmBookingOwner = require('./sendConfirmBookingOwner')
 
 module.exports = {
     sendActivationCode,
     sendBookingOfferPenddingTenant,
-    sendBookingOfferPenddingOwner
+    sendBookingOfferPenddingOwner,
+    sendConfirmBookingTenant,
+    sendConfirmBookingOwner
 }
