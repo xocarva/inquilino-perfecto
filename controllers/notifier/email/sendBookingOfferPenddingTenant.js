0d9496e5521d3320e46d64ad199df877e81921fb
davidfdzmorilla <davidfdzmorilla@gmail.com>
2022-01-07 11:10:06 +0100

    pull

--- controllers/notifier/email/sendBookingOfferPenddingTenant.js
+++ controllers/notifier/email/sendBookingOfferPenddingTenant.js
@@ -1,6 +1,7 @@
 const transporter = require('./transporter')
 const { SENDER_EMAIL } = process.env
 
+<<<<<<< Updated upstream
 const sendBookingOfferPenddingTenant = async (bookingData) => {
   const { emailTenant, startDate, endDate } = bookingData
     await transporter.sendMail({
@@ -13,3 +14,19 @@ const sendBookingOfferPenddingTenant = async (bookingData) => {
 }
 
 module.exports = sendBookingOfferPenddingTenant
+=======
+const sendBookingOfferPenddingTennant = async (bookingData) => {
+  const email = bookingData.emailTenant.email
+  const { startDate, endDate } = bookingData
+
+
+    await transporter.sendMail({
+        from: `${SENDER_EMAIL} <${SENDER_EMAIL}>`,
+        to: email,
+        subject: "Booking offer is pendding",
+        html: `<p>Your offer booking of ${startDate} at ${endDate} is pendding</p>`
+    })
+}
+
+module.exports = sendBookingOfferPenddingTennant
+>>>>>>> Stashed changes
