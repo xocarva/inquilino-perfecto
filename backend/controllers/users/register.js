88a86db7fd2c0a9089ca80364c41009b27c758dc
xocarva <correo@xoancarneiro.gal>
2022-02-07 10:24:16 +0100

    add avatar load to login

--- backend/controllers/users/register.js
+++ backend/controllers/users/register.js
@@ -1,18 +1,41 @@
 const crypto = require('crypto')
 const encryptor = require('../../shared/encryptor')
+const fs = require('fs-extra')
 const { userValidator } = require('../../validators')
 const { usersRepository } = require('../../repository')
 const notifier = require('../../controllers/notifier')
+const uploads = require('../../shared/uploads')
 
+const { MAX_IMAGE_SIZE_IN_BYTES, ALLOWED_MIMETYPES, UPLOADS_PATH } =  process.env
 
 const register = async (req, res) => {
     const user = req.body
+
+    if(!req.files || !req.files.picture) {
+        res.status(400)
+        res.end('[picture] is required')
+        return
+    }
+
+    const picture = req.files.picture
 
     try {
         await userValidator.validateAsync(user)
     } catch (error) {
         res.status(401)
         res.end(error.message)
+        return
+    }
+
+    if (!uploads.isValidImageSize(picture.size)) {
+        res.status(400)
+        res.end(`Avatar size should be less than ${MAX_IMAGE_SIZE_IN_BYTES / 1000000} Mb`)
+        return
+    }
+
+    if (!uploads.isValidImageMimeType(picture.mimetype)) {
+        res.status(400)
+        res.end(`Avatar should be ${ALLOWED_MIMETYPES.map(getExtensionFromMimetype).join(', ')}`)
         return
     }
 
@@ -38,13 +61,27 @@ const register = async (req, res) => {
     } catch (error) {
         res.status(404)
         res.end(error.message)
-    return
+        return
     }
 
     const activationCode = crypto.randomBytes(40).toString('hex')
 
+    let pictureUrl
     try {
-        await usersRepository.saveUser({ ...user, password: encryptedPassword, activationCode })
+        const pictureName = uploads.createImageName(uploads.getExtensionFromMimetype(picture.mimetype))
+        pictureUrl = `/${pictureName}`
+        fs.ensureDir(UPLOADS_PATH)
+        picture.mv(`${UPLOADS_PATH}/${pictureName}`)
+    } catch {
+        res.status(400)
+        res.end(error.message)
+        return
+    }
+
+
+    let userId
+    try {
+        userId = await usersRepository.saveUser({ ...user, password: encryptedPassword, activationCode, picture: pictureUrl  })
     } catch (error) {
         res.status(401)
         res.end(error.message)
@@ -60,7 +97,10 @@ const register = async (req, res) => {
     }
 
     res.status(201)
-    res.send('User registered and validation email sent')
+    res.send({
+        message: 'User registered and validation email sent',
+        id: userId
+    })
 
 }
 
---
+++ backend/shared/uploads.js
@@ -0,0 +1,31 @@
+const crypto = require('crypto')
+const {MAX_IMAGE_SIZE_IN_BYTES, ALLOWED_MIMETYPES, UPLOADS_PATH} =  process.env
+
+const isValidImageSize = (size) => {
+    return size <= MAX_IMAGE_SIZE_IN_BYTES
+}
+
+  const isValidImageMimeType = (mimetype) => {
+    return ALLOWED_MIMETYPES.includes(mimetype.toLowerCase())
+}
+
+  const getExtensionFromMimetype = (mimetype) => {
+    return mimetype.split('/')[1]
+}
+
+  const removeFile = async (fileName) => {
+    fs.remove(`${UPLOADS_PATH}/${fileName}`)
+}
+
+  const createImageName = (extension) => {
+    const randomHash = crypto.randomBytes(15).toString('hex')
+    return `${randomHash}.${extension}`
+}
+
+module.exports = {
+    isValidImageSize,
+    isValidImageMimeType,
+    getExtensionFromMimetype,
+    removeFile,
+    createImageName
+}
