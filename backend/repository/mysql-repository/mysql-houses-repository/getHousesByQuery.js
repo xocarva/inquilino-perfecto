2c3d02b568c4b36e02ae7d96bcfec4484f3f3dd7
xocarva <correo@xoancarneiro.gal>
2022-02-08 17:28:03 +0100

    add title to house object in getHousesByQuery

--- backend/repository/mysql-repository/mysql-houses-repository/getHousesByQuery.js
+++ backend/repository/mysql-repository/mysql-houses-repository/getHousesByQuery.js
@@ -1,7 +1,7 @@
 const connection = require('../mysqlConnection')
 
 const getHousesByQuery = async (city, price, rooms) => {
-    let query = 'SELECT id, city, title, price, rooms FROM houses'
+    let query = 'SELECT id, title, city, price, rooms FROM houses'
     const params = []
     if (city || price || rooms) query += ' WHERE'
 
