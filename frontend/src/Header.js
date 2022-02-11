b93588152fff5f4c54a97c52428d3fc8091787ac
davidfdzmorilla <davidfdzmorilla@gmail.com>
2022-02-11 19:08:15 +0100

    editProfile & tenantProfile is ok

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,23 +1,14 @@
 import './Header.css'
 import { Link } from 'react-router-dom'
-import Login from './Login'
-import { useModal, useSetModal } from './hooks'
 
 
 function Header() {
-    const setModal = useSetModal()
-    const modal = useModal()
-    const handleLogin = e => {
-        setModal(<Login/>)
-        modal(true)
-    }
 
     return(
         <header className="header">
             <Link to="/"><h1>Inquilino perfecto</h1></Link>
             <Link to="/register">Registro</Link>
             <Link to="/user/edit-profile">Editar perfil</Link>
-            <span onClick={handleLogin}>Login</span>
         </header>
     )
 }
