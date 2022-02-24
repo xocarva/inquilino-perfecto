2af08a2bf7d065b3a263bcc55b5f246062ab0bb8
henry <henry.c2405@gmail.com>
2022-02-24 10:18:54 +0100

    update new ad & owner profile

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,7 +2,7 @@ import './Home.css'
 function Home() {
     return (
         <div className='home-contenedor'>
-            <div className='image-home' style={{backgroundImage:`url("http://localhost:3001/home-picture.jpeg")`}}>
+            <div className='image-home' style={{backgroundImage:`url("https://st2.depositphotos.com/1041088/7766/i/950/depositphotos_77665926-stock-photo-large-modern-house-with-stone.jpg")`}}>
             {/* <div className='image-home' style={{backgroundImage:`url("http://localhost:3001/house-home.jpg")`}}> */}
             </div>
         </div>
